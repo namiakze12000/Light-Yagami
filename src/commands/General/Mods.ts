175e91290c05d647b9a08a506b71bba49b707478
namiakze12000 <103877545+namiakze12000@users.noreply.github.com>
2022-05-03 14:25:22 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n🌟 Chitoge 🌟 `
+        text += `\n👺 ＬＩＧＨＴ�ＹＡＧＡＭＩ 👺 `
         return void M.reply(text)
     }
 }
